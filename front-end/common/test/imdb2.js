18b26e80f82cedf3993d557664b8d57f10c0d6ed
debiandebiandebian <debiandebiandebiandebian@gmail.com>
2019-10-06 03:15:37 +0900

    Make an episode object for test purposes.

--- front-end/common/test/imdb2.js
+++ front-end/common/test/imdb2.js
@@ -1,5 +1,6 @@
 
 const imdb = require("../imdb.js");
+const uuidv1 = require("uuid/v1");
 
 (async () => {
     try {
@@ -13,14 +14,25 @@ const imdb = require("../imdb.js");
             "Wilfred",
             "The Imitation Game"
         ];
-        let ret = {};
+        let s = {};
+        let e = {};
         for (let title of titles) {
             console.log(title);
             let id = await imdbapi.find_series_id(title);
             let series_info = await imdbapi.get_series_info(id);
-            ret[id] = series_info;
+            s[id] = series_info;
+            if (s[id]["type"] == "TV Series") {
+                s[id]["episode"] = [];
+                for (let i=1; i<=5; i++) {
+                    let uuid = uuidv1();
+                    let episode_info = await imdbapi.get_episode_info(id, 1, i);
+                    e[uuid] = episode_info;
+                    s[id]["episode"].push(uuid);
+                }
+            }
         }
-        console.log(JSON.stringify(ret));
+        console.log(JSON.stringify(s));
+        console.log(JSON.stringify(e));
     } catch (e) {
         console.log(e);
     } 
